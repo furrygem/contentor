fbbab5449378c0b84fa9ba6d16653648b1b76d31
furrygem <85435415+furrygem@users.noreply.github.com>
2022-08-04 01:29:04 +0300

    Add nginx and basic BL to sharing service

--- sharing-service/src/index.ts
+++ sharing-service/src/index.ts
@@ -10,23 +10,42 @@ const PORT = process.env.PORT || 3000
 const app = express()
 
 
+type URLInfo = {
+  owner: string;
+  internalID: string;
+}
+
+
 const rcon = redis.createClient({
   'url': 'redis://redis:6379'
 })
 
+
+app.get('/:link', async (req, res) => {
+  let exists = await rcon.exists(req.params.link)
+  if (!exists) {
+    res.status(404)
+    res.send("not found")
+    return
+  }
+  let val = await rcon.hGetAll(req.params.link) as URLInfo
+  console.log(val)
+  res.setHeader("X-Accel-Redirect", `/api/objects/${val.internalID}`)
+  res.send()
+  return
+})
+
+app.get("/test/internal/:filename", (req, res) => {
+  res.setHeader("X-Accel-Redirect", `/api/objects/${req.params.filename}`)
+  res.send()
+})
+
+rcon.on('error', (err) => console.log('Redis Client Error', err))
+
+
 rcon.connect().then( () => {
-  console.log("connected to redis")
-  rcon.set("test", "test")
-
-})
-
-
-
-app.get('/:link', (req, res) => {
-  res.send({"link": req.params.link})
-})
-
-
-app.listen(PORT, () => {
-  console.log(`Starting listener on port ${PORT}`)
+  console.log("Connected to redis")
+  app.listen(PORT, () => {
+    console.log(`Starting listener on port ${PORT}`)
+  })
 })
